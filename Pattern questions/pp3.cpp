e3d513b70f58348def245a9bc039a104ab7a3f5d
Komal-J04 <komal.jain2k04@gmail.com>
2023-03-23 23:44:21 +0530

    Day2(Pattern questions based on loops)

---
+++ Pattern questions/pp3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int j;
+        for(j=1;j<=n;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+//For n=4
+// 1 2 3 4 
+// 1 2 3 4 
+// 1 2 3 4 
+// 1 2 3 4
