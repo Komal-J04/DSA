655a6fd90bce46aa2030af3aa0160275e007593a
Komal-J04 <komal.jain2k04@gmail.com>
2023-04-05 23:30:45 +0530

    Day10(Sort 012)

---
+++ CodeStudio Questions/Sort012.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+   return sort(arr,arr+n);
+}
+
+//1. Count 0's=k   1's=l   2's=m  and make a new array with k 0's, l 1's & m 2's
+//2. Sort the array
+//3. 2 pointer approach
