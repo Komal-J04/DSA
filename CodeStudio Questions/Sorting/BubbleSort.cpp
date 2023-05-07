398c05c3ac6ab7b53c1d765faddc75a6cd9e04f7
Komal-J04 <komal.jain2k04@gmail.com>
2023-05-07 23:28:55 +0530

    Day19(Bubble Sort)

---
+++ CodeStudio Questions/Sorting/BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i=0;i<n ;i++)
+    {
+        int maxi=arr[i];
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+        }
+    }
+}
+
+//more optimised
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+    bool swapped = false;
+    for(int i=0;i<n ;i++)
+    {
+        int maxi=arr[i];
+        for(int j=0;j<n-i-1;j++)
+        {
+          if (arr[j] > arr[j + 1]) 
+          {
+            swap(arr[j], arr[j + 1]);
+            swapped = true;
+          }
+        }
+        if(swapped==false) break; //alread sorted
+    }
+}
