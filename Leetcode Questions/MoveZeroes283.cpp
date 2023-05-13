e52874f8ec214cb8f3f9fcf9e0b820a72a9009be
Komal-J04 <komal.jain2k04@gmail.com>
2023-05-13 23:44:27 +0530

    Day23(Arrays - Move Zeroes)

--- Leetcode Questions/MoveZeroes283.cpp
+++ Leetcode Questions/MoveZeroes283.cpp
@@ -1,7 +1,49 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main()
+//NOT AN IN-PLACE SORT
+void moveZeroes(vector<int>& nums) 
 {
-    
+        int n=nums.size();
+        vector<int>arr;
+        int count = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                arr.push_back(nums[i]);
+            }
+            else count++;
+        }
+
+        int k=arr.size();
+        for(int i=k;i<n;i++)
+        {
+            arr.push_back(0);
+        }
+
+        nums.clear();
+        for(int p=0;p<n;p++)
+        {
+            nums.push_back(arr[p]);
+        }       
+}
+
+
+//IN PLACE SORT
+void moveZeroes(vector<int>& nums) 
+{
+    int i=0,j=0;
+    int n=nums.size();
+    while(j<n)
+    {
+        if(nums[j]==0) j++;
+        else
+        {
+            swap(nums[i],nums[j]);
+            i++;
+            j++;
+        }
+    }
 }
