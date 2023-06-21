6aa22bf6efb887f16117db884665dd0124704db7
Komal-J04 <komal.jain2k04@gmail.com>
2023-06-21 22:38:48 +0530

    Day41(Recursion)

---
+++ Recursion/BasicRecursion.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    //base case
+    if (n==0) return 1;
+    
+    //recursive relation
+    return n*fact(n-1);
+}
+
+
+int power2(int n)
+{
+    if(n==0) return 1;
+
+    return 2*power2(n-1);
+}
+
+void printCount(int n)
+{
+    //Base case
+    if(n==0) return;
+
+    //Processing
+    cout<<n<<" ";
+
+    //Recursive relation
+    printCount(n-1);
+}
+
+int fibbonacci(int n)
+{
+    if(n==1) return 0;
+    if(n==2) return 1;
+
+    return fibbonacci(n-1) + fibbonacci(n-2);
+}
+
+bool checkSorted(int arr[],int idx, int n)
+{
+    //base case
+    if(idx == n-1) return true;
+
+    return checkSorted(arr,idx+1,n) && (arr[idx]<arr[idx+1]);
+
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter number - ";
+    cin>>n;
+    cout<<n<<" factorial = "<<fact(n)<<endl;
+    cout<<"2^"<<n<<" = "<<power2(n)<<endl;
+    printCount(n);
+    cout<<endl;
+    cout<<n<<"th number in fibonacci series = "<<fibbonacci(n)<<endl;
+
+    int arr[] = {1,2,3,6,5};
+    cout<<"Array is sorted? - "<<checkSorted(arr,0,5);
+}
