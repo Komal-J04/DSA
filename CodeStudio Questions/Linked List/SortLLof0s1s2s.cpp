d56cea3f93cc80e592ea9d0865058537bdf24f2c
Komal-J04 <komal.jain2k04@gmail.com>
2023-11-01 22:20:15 +0530

    Day77(Sort LL without data manipulation)

--- CodeStudio Questions/Linked List/SortLLof0s1s2s.cpp
+++ CodeStudio Questions/Linked List/SortLLof0s1s2s.cpp
@@ -68,3 +68,60 @@ Node *sortList(Node *head)
 
     return head;
 }
+
+// SOLUTION 2 - NO DATA MANIPULATION
+void populate(Node *&tail, Node *curr)
+{
+    tail->next = curr;
+    tail = curr;
+}
+
+Node *sortList(Node *head)
+{
+    Node *zeroHead = new Node(-1);
+    Node *zeroTail = zeroHead;
+    Node *oneHead = new Node(-1);
+    Node *oneTail = oneHead;
+    Node *twoHead = new Node(-1);
+    Node *twoTail = twoHead;
+
+    Node *curr = head;
+
+    // 3 separate linked lists
+    while (curr != NULL)
+    {
+        int value = curr->data;
+
+        if (value == 0)
+            populate(zeroTail, curr);
+        if (value == 1)
+            populate(oneTail, curr);
+        if (value == 2)
+            populate(twoTail, curr);
+
+        curr = curr->next;
+    }
+
+    // merge the 3 linked lists
+    if (oneHead->next != NULL) // 1 list is non empty
+    {
+        zeroTail->next = oneHead->next;
+        oneTail->next = twoHead->next;
+        twoTail->next = NULL;
+    }
+    else // 1 list is empty
+    {
+        zeroTail->next = twoHead->next;
+        twoTail->next = NULL;
+    }
+
+    // set head
+    head = zeroHead->next;
+
+    // delete dummy nodes
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    return head;
+}
