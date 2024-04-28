2c1bb5d064ed01ef975dc373d0660b25fc38c364
Komal-J04 <komal.jain2k04@gmail.com>
2024-04-28 11:01:07 +0530

    Delete middle of linked list

---
+++ POTD/GFG/Delete-Middle-of-Linked-List.cpp
@@ -0,0 +1,103 @@
+// https://www.geeksforgeeks.org/problems/delete-middle-of-linked-list/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+// } Driver Code Ends
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution
+{
+public:
+    Node *deleteMid(Node *head)
+    {
+        // Your Code Here
+        Node *slow = head;
+        Node *temp = head;
+        Node *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL) // end->next is necessary because if end->next is NULL then we cannot access end->next->next; it will throw sigsegv
+        {
+            temp = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        if (fast->next != NULL)
+        {
+            temp = slow;
+            slow = slow->next;
+        }
+
+        temp->next = slow->next;
+
+        slow->next = NULL;
+        delete slow;
+
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int data;
+        cin >> data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            cin >> data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution obj;
+        head = obj.deleteMid(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
