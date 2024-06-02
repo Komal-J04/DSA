e90aaa2d00bc476dd795f9b616c1822a3a9ad941
Komal-J04 <komal.jain2k04@gmail.com>
2024-06-02 15:42:05 +0530

    Score of a string

--- Leetcode Questions/Binary and decimal system, bitwise operators/PowerOfTwo231.cpp
+++ Leetcode Questions/Binary and decimal system, bitwise operators/PowerOfTwo231.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int ans=1,p=0;
-    for(int i=0;i<32;i++){
-        if(ans==n) {
-            cout<<"true";
+    cin >> n;
+    int ans = 1, p = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        if (ans == n)
+        {
+            cout << "true";
             break;
         }
-        if(ans<INT_MAX/2){
-            ans=ans*2;
+        if (ans < INT_MAX / 2)
+        {
+            ans = ans * 2;
             p++;
         }
     }
-    if(p>=31) cout<<"no";
-
-    //Approach 2(Better approach) - If number of set bits=1 then the no. is divisible by 2
-    
+    if (p >= 31)
+        cout << "no";
 }
