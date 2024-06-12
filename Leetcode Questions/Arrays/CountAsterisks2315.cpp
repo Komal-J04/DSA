18471c60e2bc68bf9348e014a5550f3ac615a54c
Komal-J04 <komal.jain2k04@gmail.com>
2024-06-12 23:19:38 +0530

    Count Asterisks

---
+++ Leetcode Questions/Arrays/CountAsterisks2315.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int countAsterisks(string s)
+    {
+        int ans = 0;
+
+        int count = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '|')
+                count++;
+
+            if (count % 2 == 0 && s[i] == '*')
+                ans++;
+        }
+        return ans;
+    }
+};
