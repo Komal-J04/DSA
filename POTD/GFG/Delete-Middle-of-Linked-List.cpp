098ddb8aa3cfa06dff9e64eedfc0abb6c2ec46c7
Komal-J04 <komal.jain2k04@gmail.com>
2024-07-02 11:54:10 +0530

    Working with middle node of a LL

--- POTD/GFG/Delete-Middle-of-Linked-List.cpp
+++ POTD/GFG/Delete-Middle-of-Linked-List.cpp
@@ -101,3 +101,112 @@ int main()
 }
 
 // } Driver Code Ends
+
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+
+// APPROACH 2
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+// } Driver Code Ends
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution
+{
+public:
+    Node *deleteMid(Node *head)
+    {
+        // Your Code Here
+        if (head == NULL || head->next == NULL)
+            return NULL;
+        Node *fast = head;
+        Node *slow = head;
+        Node *prev = NULL;
+
+        while (fast->next != NULL)
+        {
+            fast = fast->next;
+            if (fast->next != NULL)
+                fast = fast->next;
+            prev = slow;
+            slow = slow->next;
+        }
+
+        prev->next = slow->next;
+        delete slow;
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int data;
+        cin >> data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            cin >> data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution obj;
+        head = obj.deleteMid(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
