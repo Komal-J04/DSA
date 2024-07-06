7bc6d12c8db4f4809625fbd89b6d7cd89da3c222
Komal-J04 <komal.jain2k04@gmail.com>
2024-07-07 00:35:17 +0530

    Delete Node in a Linked List

---
+++ Leetcode Questions/Linked List/DeleteNodeInALinkedList237.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        ListNode *nextNode = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete nextNode;
+    }
+};
+
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+// *********************************************************************************
+
+// APPROACH 2 - Since the previous linked list is pointing to the current node, this will only be traversed. We are basically replacing the next node by the current node.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
