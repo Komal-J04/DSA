61b80a0d27d50fb1468f91301da0c84cfef324ef
Komal-J04 <komal.jain2k04@gmail.com>
2024-09-14 14:32:45 +0530

    Alternate positive and negative numbers

---
+++ POTD/GFG/Alternate-positive-and-negative-numbers.cpp
@@ -0,0 +1,75 @@
+// https://www.geeksforgeeks.org/problems/array-of-alternate-ve-and-ve-nos1401/1
+
+// APPROACH 1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    void rearrange(vector<int> &arr)
+    {
+        // code here
+        vector<int> pos, neg;
+
+        for (int x : arr)
+        {
+            if (x >= 0)
+                pos.push_back(x);
+            else
+                neg.push_back(x);
+        }
+
+        int i = 0, j = 0, k = 0, po = pos.size(), ne = neg.size();
+
+        while ((j < po) && (k < ne))
+        {
+            if ((i % 2) == 0)
+                arr[i++] = pos[j++];
+            else
+                arr[i++] = neg[k++];
+        }
+
+        while (j < po)
+            arr[i++] = pos[j++];
+
+        while (k < ne)
+            arr[i++] = neg[k++];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num)
+        {
+            arr.push_back(num);
+        }
+        Solution ob;
+        ob.rearrange(arr);
+        for (int i = 0; i < arr.size(); i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
